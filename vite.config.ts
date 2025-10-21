88519dd7b84ab1d8ba5032cc496da541204899b2
Luisa Vaca <luisavaca@Ordenador-portatil-de-Luisa.local>
2025-10-21 15:25:18 +0200

    feat(Vite): add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [ vue() ],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   build: {
-    // Directorio de salida, coincidiendo con lo que usa Nuxt y el workflow.
     outDir: '.output/public',
     lib: {
-      // El punto de entrada de nuestra librería (el script que se embeberá).
       entry: path.resolve(__dirname, 'src/entry.ts'),
-      // El nombre global que expondrá la librería.
       name: 'SpalopiaWidget',
-      // El nombre del archivo de salida.
-      fileName: 'spalopia-widget',
-      // El formato de salida (UMD es universal y funciona en navegadores).
+      fileName: () => 'spalopia-widget.js',
       formats: ['umd'],
     },
   },
